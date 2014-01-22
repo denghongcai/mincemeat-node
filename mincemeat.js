0fe5c4b904f70721bd846020a8f602c96acc295b
denghongcai <admin@dhchouse.com>
2014-01-22 23:26:20 +0800

    test?

--- mincemeat.js
+++ mincemeat.js
@@ -38,25 +38,26 @@ function Protocol() {
     this.auth = undefined;
     this.mid_command = false;
     this.terminator = "";
-    var self = this;
+    
 
     this.init = function (conn) {
+        var self = this;
         if (typeof conn !== "undefined") {
-            self.session = net.createServer(function (sock) {
+            this.session = net.createServer(function (sock) {
                 sock.setEncoding("utf8");
                 sock.on('data', function (data) {
                     sock.pause();
                     //Process stream data
-                    self.buffer = self.buffer.concat(buffer);
+                    self.buffer = self.buffer.concat(data);
                     self.process_data();
                     sock.resume();
                 });
             }).listen(DEFAULT_PORT, "127.0.0.1");
         }
         else {
-            self.session = new net.Socket();
+            this.session = new net.Socket();
         }
-        self.terminator = "\n";
+        this.terminator = "\n";
     };
 
     this.send_command = function (command, data) {
@@ -77,34 +78,34 @@ function Protocol() {
 
     this.process_data = function(){
         var data;
-        if(typeof self.terminator === "number"){
-            if(self.buffer.length >= self.terminator){
-                data = self.buffer.slice(0, self.terminator);
-                self.found_terminator(data);
-                self.buffer = self.buffer.slice(self.terminator);
-                self.process_data();
+        if(typeof this.terminator === "number"){
+            if(this.buffer.length >= this.terminator){
+                data = this.buffer.slice(0, this.terminator);
+                this.found_terminator(data);
+                this.buffer = this.buffer.slice(this.terminator);
+                this.process_data();
             }
         }
         else{
-            if(self.buffer.indexOf(self.terminator) === -1){
+            if(this.buffer.indexOf(this.terminator) === -1){
 
             }
             else{
-                data = self.buffer.slice(0, self.buffer.indexOf(self.terminator));
-                self.found_terminator(data);
-                self.buffer = self.buffer.slice(self.buffer.indexOf(self.terminator) + 1);
-                self.process_data();
+                data = this.buffer.slice(0, this.buffer.indexOf(this.terminator));
+                this.found_terminator(data);
+                this.buffer = this.buffer.slice(this.buffer.indexOf(this.terminator) + 1);
+                this.process_data();
             }
         }
     };
 
     this.found_terminator = function (data) {
         var command;
-        if (self.auth != "Done") {
+        if (this.auth != "Done") {
             data = (data.split(":", 2));
             command = data[0];
             data = data[1];
-            self.process_unauthed_command(command, data);
+            this.process_unauthed_command(command, data);
         }
         else if (this.mid_command === false) {
             logger.error("-> %s", data);
@@ -112,46 +113,46 @@ function Protocol() {
             command = data[0];
             var length = data[1];
             if (command === "challenge") {
-                self.process_command(command, length);
+                this.process_command(command, length);
             }
             else if (length) {
-                self.set_terminator(parseInt(length));
-                self.mid_command = command;
+                this.set_terminator(parseInt(length));
+                this.mid_command = command;
             }
             else {
-                self.process_command(command);
+                this.process_command(command);
             }
         }
         else {
-            if (self.auth != "Done") {
+            if (this.auth != "Done") {
                 logger.error("Recieved pickled data from unauthed source");
                 process.exit();
             }
             data = pickle.loads(data);
-            self.terminator = "\n";
-            command = self.mid_command;
-            self.mid_command = false;
-            self.process_command(command, data);
+            this.terminator = "\n";
+            command = this.mid_command;
+            this.mid_command = false;
+            this.process_command(command, data);
         }
     };
 
     this.send_challenge = function () {
-        self.auth = crypto.randomBytes(20).toString("hex");
-        self.send_command(["challenge", self.auth].join(":"));
+        this.auth = crypto.randomBytes(20).toString("hex");
+        this.send_command(["challenge", this.auth].join(":"));
     };
 
     this.respond_to_challenge = function (command, data) {
-        var hmac = crypto.createHmac("sha1", self.password, data);
-        hmac.update(self.auth);
-        self.send_command(["auth", hmac.digest("hex")].join(":"));
-        self.post_auth_init();
+        var hmac = crypto.createHmac("sha1", this.password, data);
+        hmac.update(this.auth);
+        this.send_command(["auth", hmac.digest("hex")].join(":"));
+        this.post_auth_init();
     };
 
     this.verify_auth = function (command, data) {
-        var hmac = crypto.createHmac("sha1", self.password);
-        hmac.update(self.auth);
+        var hmac = crypto.createHmac("sha1", this.password);
+        hmac.update(this.auth);
         if (data == hmac.digest("hex")) {
-            self.auth = "Done";
+            this.auth = "Done";
             logging.info("Authenticated other end");
         }
         else {
@@ -184,53 +185,53 @@ Client.prototype = new Protocol();
 Client.prototype.constructor = Client;
 
 Client.prototype.conn = function (server, port) {
-    self.client = new net.Socket();
-    self.client.connect(port, server);
+    this.client = new net.Socket();
+    this.client.connect(port, server);
 };
 
 Client.prototype.set_mapfn = function (command, mapfn) {
-    self.mapfn = superJson.create().parse(mapfn);
+    this.mapfn = superJson.create().parse(mapfn);
 };
 
 Client.prototype.set_collectfn = function (command, collectfn) {
-    self.collectfn = superJson.create().parse(collectfn);
+    this.collectfn = superJson.create().parse(collectfn);
 };
 
 Client.prototype.set_reducefn = function (command, reducefn) {
-    self.reducefn = superJson.create().parse(reducefn);
+    this.reducefn = superJson.create().parse(reducefn);
 };
 
 Client.prototype.call_mapfn = function (command, data) {
     logger.info("Mapping %s", data[0].toString());
     var results = {};
-    var result = self.mapfn(data[0], data[1]);
+    var result = this.mapfn(data[0], data[1]);
     for (var k in result) {
         if (!(result[k][0] in results)) {
             results[result[k][0]] = [];
         }
         results[k].append(result[k][1]);
     }
-    if (typeof self.collectfn !== "undefined") {
+    if (typeof this.collectfn !== "undefined") {
         for (var k in results) {
-            results[k] = [self.collectfn(num, results[k])];
+            results[k] = [this.collectfn(num, results[k])];
         }
     }
-    self.send_command('mapdone', [data[0], results]);
+    this.send_command('mapdone', [data[0], results]);
 };
 
 Client.prototype.call_reducefn = function (command, data) {
     logger.info("Reducing %s", data[0].toString());
-    var results = self.reducefn(data[0], data[1]);
-    self.send_command('reducedone', [data[0], results]);
+    var results = this.reducefn(data[0], data[1]);
+    this.send_command('reducedone', [data[0], results]);
 };
 
 Client.prototype.process_command = function (command, data) {
     commands = {
-        'mapfn': self.set_mapfn,
-        'collectfn': self.set_collectfn,
-        'reducefn': self.set_reducefn,
-        'map': self.call_mapfn,
-        'reduce': self.call_reducefn
+        'mapfn': this.set_mapfn,
+        'collectfn': this.set_collectfn,
+        'reducefn': this.set_reducefn,
+        'map': this.call_mapfn,
+        'reduce': this.call_reducefn
     };
 
     if (command in commands) {
@@ -242,14 +243,14 @@ Client.prototype.process_command = function (command, data) {
 };
 
 Client.prototype.post_auth_init = function () {
-    if (self.auth === undefined) {
-        self.send_challenge();
+    if (this.auth === undefined) {
+        this.send_challenge();
     }
 };
 
 function Server() {
-    this.datasource = undefined;
-    var self = this;
+    this._datasource = undefined;
+    
     this.socket_map = [];
     this.mapfn = undefined;
     this.reducefn = undefined;
@@ -257,8 +258,12 @@ function Server() {
     this.password = undefined;
 
     this.run_server = function (password, port) {
+        var self = this;
         net.createServer(function(sock){
             self.socket_map.push(sock);
+            sock.on("data", function(){
+                var sc = ServerChannel();
+            });
             sock.on("close", function(){
                 logger.warn("CLIENT CLOSED:" + sock.remoteAddress + " " + sock.remotePort);
             })
@@ -266,26 +271,25 @@ function Server() {
     };
 
     this.set_datasource = function (ds) {
-        self._datasource = ds;
-        self.taskmanager = new TaskManager(self._datasource);
+        this._datasource = ds;
+        this.taskmanager = new TaskManager(this._datasource);
     };
 
     this.get_datasource = function () {
-        return self._datasource;
+        return this._datasource;
     };
 
-    this.prototype = {
-        get datasource() {
-            return self._datasource;
-        },
-        set datasource(val) {
-            self._datasource = val;
-        }
-    };
+    this.prototype.__defineSetter__("datasource", function(val){
+        this._datasource = val;
+    });
+
+    this.prototype.__defineGetter__("datasource", function(){
+        return this._datasource;
+    });
 }
 
 function ServerChannel(server) {
-    var self = this;
+    
     this.init(conn);
     this.server = server;
     this.start_auth();
@@ -296,56 +300,56 @@ ServerChannel.prototype = new Protocol();
 ServerChannel.prototype.constructor = ServerChannel;
 
 ServerChannel.prototype.start_auth = function () {
-    self.send_challenge();
+    this.send_challenge();
 };
 
 ServerChannel.prototype.start_new_task = function () {
-    var data = self.server.taskmanager.next_task();
+    var data = this.server.taskmanager.next_task();
     if (data[0] == "undefined") {
         return
     }
-    self.send_command(data[0], data[1]);
+    this.send_command(data[0], data[1]);
 };
 
 ServerChannel.prototype.mapdone = function (command, data) {
-    self.server.taskmanager.map_done(data);
-    self.start_new_task();
+    this.server.taskmanager.map_done(data);
+    this.start_new_task();
 };
 
 ServerChannel.prototype.reducedone = function (command, data) {
-    self.server.taskmanager.reduce_done();
-    self.start_new_task();
+    this.server.taskmanager.reduce_done();
+    this.start_new_task();
 };
 
 ServerChannel.prototype.process_command = function (command, data) {
     var commands = {
-        'mapdone': self.map_done,
-        'reducedone': self.reduce_done
+        'mapdone': this.map_done,
+        'reducedone': this.reduce_done
     };
 
     if (command in commands) {
         commands[command](command, data)
     }
     else {
-        Protocol.process_command(command, data);
+        Protocol.prototype.process_command.call(this, command, data);
     }
 };
 
 ServerChannel.prototype.post_auth_init = function () {
-    if (typeof self.server.mapfn != "undefined") {
-        self.send_command('mapfn', superJson.create().stringify(self.server.mapfn));
+    if (typeof this.server.mapfn != "undefined") {
+        this.send_command('mapfn', superJson.create().stringify(this.server.mapfn));
     }
-    if (typeof self.server.reduce != "undefined") {
-        self.send_command('reducefn', superJson.create().stringify(self.server.reducefn));
+    if (typeof this.server.reduce != "undefined") {
+        this.send_command('reducefn', superJson.create().stringify(this.server.reducefn));
     }
-    if (typeof self.server.collectfn != "undefined") {
-        self.send_command('collectfb', superJson.create().stringify(self.server.collectfn));
+    if (typeof this.server.collectfn != "undefined") {
+        this.send_command('collectfb', superJson.create().stringify(this.server.collectfn));
     }
-    self.start_new_task()
+    this.start_new_task()
 };
 
 function TaskManager(datasource, server) {
-    var self = this;
+    
     this.START = 0;
     this.MAPPING = 1;
     this.REDUCING = 2;
@@ -356,78 +360,85 @@ function TaskManager(datasource, server) {
     this.state = this.START;
 
     this.next_task = function (channel) {
-        if (self.state == self.START) {
-            self.map_iter = iterator(this.datasource);
-            self.working_maps = {};
-            self.map_results = {};
-            self.state = self.MAPPING;
+        if (this.state == this.START) {
+            this.map_iter = iterator(this.datasource);
+            this.working_maps = {};
+            this.map_results = {};
+            this.state = this.MAPPING;
         }
-        if (self.state == self.MAPPING) {
+        if (this.state == this.MAPPING) {
             try {
                 var map_key = this.map_iter.next();
-                var map_item = [map_key, self.datasource[map_key]];
-                self.working_maps[map_item[0]] = map_item[1];
+                var map_item = [map_key, this.datasource[map_key]];
+                this.working_maps[map_item[0]] = map_item[1];
                 return ['map', map_item];
             }
             catch (err) {
-                if (self.working_maps.length > 0) {
+                if (this.working_maps.length > 0) {
                     var keys = [];
                     for (var k in obj) keys.push(k);
-                    return ['map', [key, self.working_maps[Math.ceil(Math.random() * (keys.length - 1))]]];
+                    return ['map', [key, this.working_maps[Math.ceil(Math.random() * (keys.length - 1))]]];
                 }
-                self.state = self.REDUCING;
-                self.reduce_iter = self.map_results.iteritems();
-                self.working_reduces = {};
-                self.results = {};
+                this.state = this.REDUCING;
+                this.reduce_iter = this.map_results.iteritems();
+                this.working_reduces = {};
+                this.results = {};
             }
         }
-        if (self.state == self.REDUCING) {
+        if (this.state == this.REDUCING) {
             try {
-                var reduce_item = self.reduce_iter.next();
-                self.working_reduces[reduce_item[0]] = reduce_item[1];
-                return ('reduce', reduce_item);
+                var reduce_item = this.reduce_iter.next();
+                this.working_reduces[reduce_item[0]] = reduce_item[1];
+                return ['reduce', reduce_item];
             }
             catch (err) {
-                if (self.working_reduces.length > 0) {
+                if (this.working_reduces.length > 0) {
                     var keys = [];
                     for (var k in obj) keys.push(k);
-                    return ['map', [key, self.working_reduces[Math.ceil(Math.random() * (keys.length - 1))]]];
+                    return ['map', [key, this.working_reduces[Math.ceil(Math.random() * (keys.length - 1))]]];
                 }
-                self.state = self.FINISHED;
+                this.state = this.FINISHED;
             }
         }
-        if (self.state == self.FINISHED) {
-            self.server.close();
+        if (this.state == this.FINISHED) {
+            this.server.close();
             return ['disconnect', undefined];
         }
     };
 
     this.map_done = function (data) {
-        if (!(data[0] in self.working_maps)) {
+        if (!(data[0] in this.working_maps)) {
             return;
         }
 
         for (var k in data[1].toArray()) {
-            if (!(k in self.map_results)) {
-                self.map_results[key] = [];
+            if (!(k in this.map_results)) {
+                this.map_results[key] = [];
             }
-            self.map_results[key].append(data[1].toArray()[k]);
+            this.map_results[key].append(data[1].toArray()[k]);
         }
 
-        delete self.working_maps[data[0]];
+        delete this.working_maps[data[0]];
     };
 
     this.reduce_done = function (data) {
-        if (!(data[0] in self.working_reduces)) {
+        if (!(data[0] in this.working_reduces)) {
             return;
         }
-        self.results[data[0]] = data[1];
-        delete self.working_reduces[data[0]];
+        this.results[data[0]] = data[1];
+        delete this.working_reduces[data[0]];
     };
 }
+
+
+var s = new Server();
+s.run_server();
 
 function run_client() {
     var client = new Client();
     client.password = "123456";
+    client.conn(8185, "127.0.0.1");
 }
+
+run_client();
 
